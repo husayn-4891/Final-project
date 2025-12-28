3fba5d43cc34527bf03f3f836d02fa4fda5a19a7
Omar Elkaleh <omar_4926@limu.edu.ly>
2025-12-29 00:21:50 +0200

    add middleware

--- app/Http/Controllers/Auth/authController.php
+++ app/Http/Controllers/Auth/authController.php
@@ -7,28 +7,37 @@ use App\Models\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class authController extends Controller
 {
-
-public function adminLogin()
+    public function adminLogin(): View
     {
         return view('Auth.admin');
     }
 
-    public function adminCheckLogin(Request $request)
+    public function adminCheckLogin(Request $request): RedirectResponse
     {
-        $credentials=$request->validate([
-            'email'=>['required','email'],
-            'password'=>['required']
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
         ]);
-        $admin=Admin::where('email',$credentials['email'])->first();
-        if($admin && Hash::check($credentials['password'],$admin->password))
-        {
-             Auth::guard('admin')->login($admin);
-             return redirect()->route('dashboard')->with('suceess','Hello'.$admin->name);
+        $admin = Admin::where('email', $credentials['email'])->first();
+        if ($admin && Hash::check($credentials['password'], $admin->password)) {
+            Auth::guard('admin')->login($admin);
+            return redirect()->route('dashboard')->with('suceess', 'Hello ' . $admin->name);
         }
-        return redirect()->back()->with('error','invalid email or password');
+        return redirect()->back()->with('error', 'invalid email or password');
     }
 
+    public function logout(Request $request): RedirectResponse
+    {
+        if (Auth::guard('admin')->check()) {
+            Auth::guard('admin')->logout();
+        }
+        $request->session()->invalidate();
+        $request->session()->regenerate();
+        return redirect('/');
+    }
 }
